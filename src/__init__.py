a7317cb466c8569843d1ea3352aeaedf9e5ec584
dzungpham <23127003@student.hcmus.edu.vn>
2025-12-13 19:58:31 +0700

    add import optimized module

--- src/__init__.py
+++ src/__init__.py
@@ -11,3 +11,13 @@ from .build import (build_unet,
                    build_style_encoder, 
                    build_content_encoder,
                    build_scr)
+from .build_optimized import (build_unet_cached, 
+                              build_style_encoder_cached,
+                              build_content_encoder_cached,
+                              build_scr_cached,
+                              build_unet_optimized,
+                              build_style_encoder_optimized,
+                              build_content_encoder_optimized,
+                              build_scr_optimized,
+                              build_ddpm_scheduler_optimized,
+                              build_ddpm_scheduler_fast)
